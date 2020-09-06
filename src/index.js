01dcb09bee7246e9551262560c0adc57568fd9ad
Duany Dreyton <duanyrf@gmail.com>
2020-09-06 19:09:22 -0300

    fix baseUrl to baseURL suggested by Joseph from Rocketseat

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import {
-  View,
   SafeAreaView,
   FlatList,
   Text,
   StatusBar,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 import api from './services/api';
 
@@ -14,18 +14,41 @@ export default function App() {
 
   useEffect(() => {
     api.get('repositories').then((response) => {
+      console.log(response.data);
       setProjects(response.data);
     });
   }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('/repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      url: 'https://github.com',
+      techs: ['NodeJS', 'React'],
+    });
+
+    setProjects([...projects, response.data]);
+  }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
 
       <SafeAreaView style={styles.container}>
-        <View>
-          <Text style={styles.title}>Hello GoStack</Text>
-        </View>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -35,13 +58,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    // justifyContent: 'center',
-    // alignItems: 'center',
   },
 
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 32,
+    fontSize: 20,
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonText: {
     fontWeight: 'bold',
+    fontSize: 16,
   },
 });
